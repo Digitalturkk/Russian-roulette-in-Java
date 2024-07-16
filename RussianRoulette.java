2aa37403a9154dcb8d64dadab7d03790085456b9
Farid <155392795+Digitalturkk@users.noreply.github.com>
2024-07-17 01:28:05 +0400

    Update RussianRoulette.java
    The is working now 👍 
    Also decided to add ArrayList util

--- RussianRoulette.java
+++ RussianRoulette.java
@@ -1,38 +1,59 @@
-import java.util.Random;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
 
 class RussianRoulette{
-    public static int ranInt(int ranNum){ //creating new random number
-        Random ranNum = new Random();
-        int ranNumb = ranNum.nextInt(6);
+    public static int ranInt(){
+        // Creating new random number as a staring point of our drum
+        Random ranNumb = new Random();
+        int ranNumber = ranNumb.nextInt(6) + 1;
+        return ranNumber;
     }
 
     public static void main(String[] args){
+        // Place of bullet in cylinder
         Random win = new Random();
-        int deathNum = win.nextInt(6); // место пули
+        int deathNum = win.nextInt(6) + 1;
 
-        int winScore, loseScore;
+        // Scores in our game
+        int winScore, gameScore;
         winScore = 0;
-        loseScore = 0;
+        gameScore = 5;
 
+        // New objects from imported libleries
         Scanner yesNo = new Scanner(System.in);
+        ArrayList<Integer> used = new ArrayList<Integer>();
 
-        while (winScore != 5 || loseScore != 1){
+        // String arrays for printing more interesting dialogs (working on it)
+
+        while (winScore != 5){
             System.out.println("Are you ready to load your revolver? y/n");
             String yOrn = yesNo.nextLine();
 
             if(yOrn.equals("y")){
-                RussianRoulette gameStart = new RussianRoulette();
-                int start = gameStart.ranInt();
-                if(start == deathNum){
-                    System.out.println("You dead");
-                    loseScore++;
-                } else {
-                    System.out.println("You won this time");
-                    // добавить счётчик оставшихся игр
-                    // Можно и добвить прокрутку
-                }
+                int start = ranInt();
+                    if (start == deathNum) {
+                        System.out.println("You dead");
+                        System.out.println("Your Win score is: " + winScore);
+                        break;
+                    } else {
+                        /*
+                        We need to be sure, that our number isn't repeating.
+                        For this, we ctreated an ArrayList (line: 25) where we will store our used random numbers.   
+                         */
+                        if (!used.contains(start)){
+                            used.add(start);
+                            System.out.println("You won this time");
+                            winScore++;
+                            gameScore -= 1;
+                            // Можно и добвить прокрутку
+                        } else {
+                            start = ranInt();
+                            System.out.println("The drum has stuck, try again >:^) ");
+                        }
+                    }
             }
+            // Print прокрутку
         }
     }
 }
