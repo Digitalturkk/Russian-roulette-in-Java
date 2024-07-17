ad789b070380dad2244f7b564678c8c614f14c48
Farid <155392795+Digitalturkk@users.noreply.github.com>
2024-07-18 00:19:10 +0400

    Update RussianRoulette.java
    Added new dialogs that make the game funnier, also add a "luck" when the drum is stuck and you don't die

--- RussianRoulette.java
+++ RussianRoulette.java
@@ -1,13 +1,19 @@
+import javax.lang.model.type.NullType;
+import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Random;
 
 class RussianRoulette{
-    public static int ranInt(){
+    public static int ranInt(int x){
         // Creating new random number as a staring point of our drum
         Random ranNumb = new Random();
-        int ranNumber = ranNumb.nextInt(6) + 1;
-        return ranNumber;
+        return ranNumb.nextInt(x) + 1;
+    }
+
+    public int noRepeat(){
+        int a = 1;
+        return a;
     }
 
     public static void main(String[] args){
@@ -24,32 +30,59 @@ class RussianRoulette{
         Scanner yesNo = new Scanner(System.in);
         ArrayList<Integer> used = new ArrayList<Integer>();
 
-        // String arrays for printing more interesting dialogs (working on it)
+        // String Array list for win dialog
+        ArrayList<String> wonDialog = new ArrayList<>();
+        wonDialog.add(0,""); // As in ranInt we have "+ 1" we couldn't get first index, so we need to add it all, and put there null string
+        wonDialog.add(1,"You are really lucky!");
+        wonDialog.add(2,"You won this time.");
+        wonDialog.add(3,"Haha, isn't it funny?");
+        wonDialog.add(4,"How Russians say 'born in a shirt' ");
+        wonDialog.add(5,"Ohh, did you shit in your pants? Ahaha, wait... did you?");
+        wonDialog.add(6,"It was so close...");
+        wonDialog.add(7,"You are dealing with death!");
+        wonDialog.add(8,"I remember, how i played this game first time, never lost till this time, ahaha");
+
+        // String Array list for lose dialog
+        ArrayList<String> loseDialog = new ArrayList<>();
+        loseDialog.add(0,""); // Same logic
+        loseDialog.add(1,"You loser");
+        loseDialog.add(2,"His body is smelling, bring him out!");
+        loseDialog.add(3,"HAHAHAHA");
+        loseDialog.add(4,"Never deal with Death.");
+        loseDialog.add(5,"Bring the other one!");
+        loseDialog.add(6,"Wanna start again?");
+        loseDialog.add(7,"I just started to like you...");
+
+        // String array list for printing more interesting dialogs. The lucky ones with drum stuck
+        String[] ld = {"", "The drum is stuck", "You lucky bitch...", "WTF IS WRONG WITH THIS GUN?", "You think that you are lucky?"};
+        ArrayList<String> luckyDialog = new ArrayList<String>(List.of(ld));
 
         while (winScore != 5){
             System.out.println("Are you ready to load your revolver? y/n");
             String yOrn = yesNo.nextLine();
 
             if(yOrn.equals("y")){
-                int start = ranInt();
+                int start = ranInt(6);
                     if (start == deathNum) {
-                        System.out.println("You dead");
+                        System.out.print("You dead. ");
+                        System.out.println(loseDialog.get(ranInt(7)));
                         System.out.println("Your Win score is: " + winScore);
                         break;
                     } else {
                         /*
                         We need to be sure, that our number isn't repeating.
-                        For this, we ctreated an ArrayList (line: 25) where we will store our used random numbers.   
+                        For this, we created an ArrayList (line: 31) where we will store our used random numbers.
                          */
                         if (!used.contains(start)){
                             used.add(start);
-                            System.out.println("You won this time");
+                            System.out.println(wonDialog.get(ranInt(8)));
+                            System.out.println("Last "+gameScore+" games >:^)");
                             winScore++;
                             gameScore -= 1;
                             // Можно и добвить прокрутку
                         } else {
-                            start = ranInt();
-                            System.out.println("The drum has stuck, try again >:^) ");
+                            start = ranInt(6);
+                            System.out.println(luckyDialog.get(ranInt(5))+", try again >:^) ");
                         }
                     }
             }
